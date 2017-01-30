44eddae5bf98114dfecdd1ac7688226ec6a9e327
jasontsh <tangsh@sas.upenn.edu>
2017-01-29 22:08:29 -0500

    wrong math :^)

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ document.body.appendChild(renderer.domElement);
 var geometry = new THREE.Geometry();
 
 geometry.vertices.push(
-	new THREE.Vector3( -10,  10, 0 ),
-	new THREE.Vector3( -10, -10, 0 ),
-	new THREE.Vector3(  10, -10, 0 )
+	new THREE.Vector3( 0,  20, 0 ),
+	new THREE.Vector3( 0, 40, 0 ),
+	new THREE.Vector3(  0, 100, 0 )
 );
 
 geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
