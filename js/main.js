675746c7e1c627cbc5d8748756d2ba36841b0d55
jasontsh <tangsh@sas.upenn.edu>
2017-01-31 19:43:01 -0500

    make mountains fatter again

--- js/main.js
+++ js/main.js
@@ -58,12 +58,12 @@ loader.load("grass.jpg", function(texture){
 
 
 generateArray(); 
-var right_plane = new THREE.PlaneGeometry(60, 300, 256, 256);
+var right_plane = new THREE.PlaneGeometry(200, 300, 256, 256);
 
 for (var i = 0; i < right_plane.vertices.length; i++) {
-	if (right_plane.vertices[i].x > -29.5) {
-		right_plane.vertices[i].z = noise((right_plane.vertices[i].x + 30)*256.0/60.0, 
-			(right_plane.vertices[i].y + 150)*256.0/300.0) * 7;
+	if (right_plane.vertices[i].x > -149.5) {
+		right_plane.vertices[i].z = noise(Math.abs(right_plane.vertices[i].x)*256.0/10.0, 
+			Math.abs(right_plane.vertices[i].y)*256.0/1000.0) * 7;;
 		if (right_plane.vertices[i].z < 0) {
 			right_plane.vertices[i].z = 0;
 		}
@@ -75,7 +75,7 @@ loader.load("grass.jpg", function(texture){
 	var right_mountains = new THREE.Mesh(right_plane, 
 		new THREE.MeshBasicMaterial({color:0x00aaaa, map: texture}));
 	scene.add(right_mountains);
-	right_mountains.position.x = 37;
+	right_mountains.position.x = 148;
 	right_mountains.rotation.x = -Math.PI / 2;
 });
 
