30a92087584438774621b941b6749aab3399c1e5
jasontsh <tangsh@sas.upenn.edu>
2017-02-01 19:03:01 -0500

    LOL

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ var render = function() {
 				splashes[j] = [Math.random() * 0.1, 
 				Math.random() * 0.2, 
 				Math.random() * 0.1,
-				new THREE.Mesh(new THREE.SphereBufferGeometry(-1 * fireworks[i][3].position.z, 30, 30), 
+				new THREE.Mesh(new THREE.SphereBufferGeometry(-0.01 * fireworks[i][3].position.z, 30, 30), 
 					new THREE.MeshBasicMaterial({color: 0xaaaaaa}))];
 				scene.add(splashes[j][3]);
 				splashes[j][3].position.x = fireworks[i][3].position.x;
