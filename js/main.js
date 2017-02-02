09d091be12b49cd7a4feedaa0d4118250dae2eb4
jasontsh <tangsh@sas.upenn.edu>
2017-02-01 19:01:33 -0500

    change speed again

--- js/main.js
+++ js/main.js
@@ -63,9 +63,9 @@ var render = function() {
 			scene.remove(spherebuffer);
 			var splashes = [];
 			for (var j = 0; j < Math.random()*3 + 3; j++){
-				splashes[j] = [Math.random() * 0.1 + fireworks[i][3].position.x, 
-				Math.random() * 0.2 + fireworks[i][3].position.y, 
-				Math.random() * 0.1 + fireworks[i][3].position.z,
+				splashes[j] = [Math.random() * 0.1, 
+				Math.random() * 0.2, 
+				Math.random() * 0.1,
 				new THREE.Mesh(new THREE.SphereBufferGeometry(-1 * fireworks[i][3].position.z, 30, 30), 
 					new THREE.MeshBasicMaterial({color: 0xaaaaaa}))];
 				scene.add(splashes[j][3]);
